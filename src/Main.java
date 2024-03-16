056dfc76ab18a74cea4bd9584a8c034bb22a5b5f
Minh Vũ <43270906+tino2002@users.noreply.github.com>
2024-03-16 14:52:01 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,47 @@
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 5, 6, 2, 7, 8, 0, 4, 3, 9, 2, 8, 1, 0, 5, 7, 4, 6, 9, 3, 2, 1, 8, 4, 0, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0, 8, 4, 6, 7, 9, 5, 3, 2, 1, 0};
-    
+        
+        Thread sumThread = new SumThread(numbers);
+        Thread maxThread = new MaxThread(numbers);
+        
+        sumThread.start();
+        maxThread.start();
+    }
+}
 
+class SumThread extends Thread {
+    private int[] numbers;
 
-        Viết mã của bạn vào đây
-    
+    public SumThread(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public void run() {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        System.out.println("Tổng của các phần tử: " + sum);
+    }
+}
+
+class MaxThread extends Thread {
+    private int[] numbers;
+
+    public MaxThread(int[] numbers) {
+        this.numbers = numbers;
+    }
+
+    @Override
+    public void run() {
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        System.out.println("Phần tử lớn nhất: " + max);
     }
 }
